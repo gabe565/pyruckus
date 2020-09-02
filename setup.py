7bed7e8a239a959d2f093899395aca827587f1f5
Gabe Cook <gabe565@gmail.com>
2020-09-02 15:21:50 -0500

    :bookmark: Bump release to 0.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as file:
 
 setup(
     name="pyruckus",
-    version="0.3",
+    version="0.4",
     description="Python API to interact with a Ruckus Unleashed device.",
     long_description=long_description,
     long_description_content_type="text/markdown",
