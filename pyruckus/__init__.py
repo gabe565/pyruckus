732d2497a9f4ac666370b1d0579cac6d9e2b1e64
ms264556 <ms264556>
2023-03-10 05:27:24 +0000

    Added comment clarifying my initial Ruckus AJAX code has a permissive 0BSD license. You can remove the comment. But it will be clear from your project's checkin history that my original code doesn't require attribution.

--- pyruckus/__init__.py
+++ pyruckus/__init__.py
@@ -18,6 +18,9 @@ from .exceptions import AuthenticationError
 
 
 class Ruckus:
+    # Copyright (C) 2023 by Patternicity bsd0@patterni.city
+    # Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted.
+    # THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
     async def get_mesh_info(self) -> dict:
         meshinfo = await self.conf("<ajax-request action='getconf' comp='mesh-list' DECRYPT_X='true'/>")
